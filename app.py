5bf1400661c7451197dd114ba7b45b242f81f0c2
root <root@aleksiatx-stage.onschrodinger.com>
2023-09-28 15:33:22 +0000

    working for aleksia

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def allowed_file(filename):
 def index():
     return render_template('upload.html')
 
-@app.route('/livedesign/images/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
     print(request.files)
     print(request.form)
@@ -37,7 +37,6 @@ def upload_file():
         return "No first level defined"
     if form['second'] == '':
         return "No second level defined"
-    
 
 
     if file and allowed_file(file.filename):
@@ -57,7 +56,7 @@ def upload_file():
 
     return "Invalid file type"
 
-@app.route('/livedesign/images/<string:first>/<string:second>/<string:file_name>')
+@app.route('/<string:first>/<string:second>/<string:file_name>')
 def serve_image_from_db(first, second, file_name):
     # Retrieve image data from the IMAGE_DB
     conn = sqlite3.connect(IMAGE_DB)
@@ -68,8 +67,6 @@ def serve_image_from_db(first, second, file_name):
 
     if result:
         frist, second, file_name, image_data = result
-        print('oh buddy')
-        print(file_name)
         return send_file(
             io.BytesIO(image_data),
             mimetype='image/jpeg',
@@ -81,20 +78,3 @@ def serve_image_from_db(first, second, file_name):
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
-
-'''
-# Adams Code
-@app.route('/livedesign/image_service/', methods=['GET'])
-def image_service():
-    args = request.args.to_dict()
-    vault_id = args['vault']
-    batch_id = args['batch']
-    protocol_id = args['protocol']
-
-    blob = plot_cache.get(vault_id, batch_id, protocol_id)
-
-    return send_file(BytesIO(blob),
-                     mimetype='image/png',
-                     as_attachment=True,
-                     download_name=f'{vault_id}_{batch_id}_{protocol_id}.png')
-'''
