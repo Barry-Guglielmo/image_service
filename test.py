f8e8f4dc47699ebf5420d3c3097fb9b538d90fc8
root <root@aleksiatx-stage.onschrodinger.com>
2023-09-29 01:14:44 +0000

    fixed supervisor user

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ data = {"first": "scinamic", "second": 'curves'}
 image_path = 'templates/rick.jpeg'
 file = {'file': ('rick.jpeg', open(image_path, 'rb'))}
 response = requests.post(API_UPLOAD_URL, files=file, data=data )
-print(response)
+print(response.text)
 
 # Test Getting the Image
 endpoint = '%s/%s/%s/%s'%(BASE_URL+'/livedesign/images',data['first'],data['second'],'rick.jpeg')
